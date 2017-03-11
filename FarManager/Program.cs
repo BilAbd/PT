4c34c8a53b348152a46d363d940162075693814d
BilAbd <bilalabdusattarov@gmail.com>
2017-03-11 14:56:57 +0600

    Add files via upload

---
+++ FarManager/Program.cs
@@ -0,0 +1,21 @@
+﻿using FarManager;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int cursor = 0;
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\HP\Documents\Visual Studio 2015");
+            qwerty.writeAll(dir, cursor);
+        }
+    }
+}
---
+++ FarManager/qwerty.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager
+{
+    public class qwerty
+    {
+        public static void writeAll(DirectoryInfo dir, int cursor)
+        {
+            Console.Clear();
+            FileSystemInfo[] qwert = dir.GetFileSystemInfos();
+            for (int i = 0; i < qwert.Length; i++)
+            {
+                Console.BackgroundColor = (i == cursor) ? ConsoleColor.Green : ConsoleColor.Blue;
+                Console.ForegroundColor = (qwert[i].GetType() == typeof(DirectoryInfo)) ? ConsoleColor.Cyan : ConsoleColor.DarkMagenta;
+                Console.WriteLine(qwert[i].Name);
+            }
+            ConsoleKeyInfo btn = Console.ReadKey();
+            switch (btn.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (cursor > 0)
+                        cursor--;
+                    writeAll(dir, cursor);
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (cursor <= qwert.Length)
+                        cursor++;
+                    writeAll(dir, cursor);
+                    break;
+                case ConsoleKey.Enter:
+                    FileSystemInfo fs = dir.GetFileSystemInfos()[cursor];
+                    if (fs.GetType() == typeof(DirectoryInfo))
+                    
+                    {
+                        dir = new DirectoryInfo(fs.FullName);
+                        cursor = 0;
+                        writeAll(dir, cursor);
+                    }
+
+                    else
+                    {
+                        StreamReader sr = new StreamReader(fs.FullName);
+                        Console.Clear();
+                        string s = sr.ReadToEnd();
+                        Console.Write(s);
+                        Console.ReadKey();
+                        writeAll(dir, cursor);
+                    }
+                    break;
+
+                case ConsoleKey.Backspace:
+                    dir = dir.Parent;
+                    writeAll(dir, cursor);
+                    break;
+                case ConsoleKey.Escape:
+                    return;
+            }
+        }
+    }
+}
+
